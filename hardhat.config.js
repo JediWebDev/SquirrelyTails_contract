d4ab8dbcc982f7e7adcec03411876b92c8d0885f
Andrew <56172745+JediWebDev@users.noreply.github.com>
2025-06-11 16:12:15 -0700

    changed hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,9 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-chai-matchers");
-require("@nomicfoundation/hardhat-verify");
 
 const networks = {
   hardhat: {
